ee7ab5aedc03e0c72c1151b893634b30f29a65c0
Alan Flores <alanif@hotmail.com>
2024-05-06 11:16:06 -0500

    feat: adding error handler and installing jsonwebtoken

--- index.ts
+++ index.ts
@@ -1,18 +1,22 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
+import { ErrorHandler } from './src/utils'
+import authRouter from './src/auth/auth.routes'
 
 dotenv.config()
 const server = express()
 const PORT = process.env.PORT
 server
   .use(express.json())
-  .get('/ping', (_, res) => {
+  .use(authRouter)
+  .get('/ping', (_, res, next) => {
     try {
       res.status(200).json({ message: 'pong' })
     } catch (err) {
-      res.status(400).json({ err })
+      next(err)
     }
   })
+  .use(ErrorHandler)
   .listen(PORT, () => {
     console.log(`Servidor corriendo en ${PORT}`)
   })
---
+++ src/auth/auth.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const authRouter = Router()
+
+authRouter
+  .get('/')
+
+
+export default authRouter
